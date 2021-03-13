30fe93e2817dea2ccd1eae815a407fe8af792b78
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-03-13 19:30:20 +0100

    add size and style features

--- scrypt.js
+++ scrypt.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("draw");
 const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
 ctx.strokeStyle = "#BADA55";
 ctx.lineJoin = "round";
 ctx.lineCap = "round";
